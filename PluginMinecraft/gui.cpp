11b201bc97e18918b52c3244bfaaec72a936aa81
bhlk64 <mahoangquyet@gmail.com>
2026-07-21 02:01:39 +0700

    5

--- PluginMinecraft/gui.cpp
+++ PluginMinecraft/gui.cpp
@@ -9,26 +9,35 @@
 
 void DrawGUI()
 {
-  LOGI("DrawGUI called");
-  glClearColor(1, 0, 0, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
-  LOGI("glError = %x", glGetError());
-  LOGI("context = %p", eglGetCurrentContext());
+  //LOGI("DrawGUI called");
 
   static bool initialized = false;
+  LOGI("%s", glGetString(GL_VERSION));
 
   if (!initialized) {
     if (eglGetCurrentContext() != EGL_NO_CONTEXT) {
       ImGui::CreateContext();
   
       ImGui_ImplOpenGL3_Init( "#version 300 es");
+      LOGI("Init = %d", ok);
+      
   
       initialized = true;
     }
   }
+  
+  EGLDisplay dpy = eglGetCurrentDisplay();
+  EGLSurface surf = eglGetCurrentSurface(EGL_DRAW);
+  
+  EGLint w, h;
+  eglQuerySurface(dpy, surf, EGL_WIDTH, &w);
+  eglQuerySurface(dpy, surf, EGL_HEIGHT, &h);
+  
   ImGuiIO& io = ImGui::GetIO();
-  LOGI("Display = %.1f %.1f", io.DisplaySize.x, io.DisplaySize.y);
-
+  io.DisplaySize = ImVec2((float)w, (float)h);
+  io.Fonts->AddFontDefault();
+  
+  LOGI("Display = %d %d", w, h);
   ImGui_ImplOpenGL3_NewFrame();
 
   ImGui::NewFrame();
